bbb6c809a1ce65e24ec6f1d7b3277e387194eb62
Andre Braga <andrebragagd@gmail.com>
2020-05-13 16:29:24 -0300

    atualizacao de alguns arquivos - nenhuma mudanca significativa

--- index.php
+++ index.php
@@ -11,18 +11,21 @@
         </script>
     </head>
     <body>
+        
         <div class="loginform">
             <div class="logo"><img src="Assets/img/logo/PDSlogo2.svg" width="400"></div>
             <div class="formarea">
-                <input class="table" type="text" placeholder="USERNAME" required><br>
-                <input class="table" type="password" placeholder="PASSWORD" required><br>
-                <div class="btns">
-                    <button class="OK" type="submit">LOGIN</button>
-                    <input type="button" class="REG" value="REGISTER" onclick="window.location.href = '/register.php'">
-                </div>
+                <form method="POST" action="index.php">
+                    <input class="table" type="text" placeholder="USERNAME" name="login" id="login">
+                    <input class="table" type="password" placeholder="PASSWORD" name="pword" id="pword">
+                    <div class="btns">
+                        <input class="OK" type="submit" value="LOGIN" id="entrar" name="entrar">
+                        <input type="button" class="REG" value="REGISTER" onclick="window.location.href = '/register.php'">
+                    </div>
+                </form>
             </div>
             <div class="disclaimer">
-                <a>#### ALL RIGHTS RESERVED ######## 2020</a>
+                <a>P.D.S. é um projeto independente</a>
             </div>
         </div>
     </body>
