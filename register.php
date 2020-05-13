625f4ce7a613289293732085f2715477857c9987
Andre Braga <andrebragagd@gmail.com>
2020-05-13 16:37:46 -0300

    alteracao em register.php - puramente estetica

--- register.php
+++ register.php
@@ -13,20 +13,22 @@
         <div class="loginform">
             <div class="logo"><img src="Assets/img/logo/PDSlogo2.svg" width="400"></div>
             <div class="formarea">
-                <input class="table" type="text" placeholder="USERNAME" required><br>
-                <input class="table" type="password" placeholder="PASSWORD" required><br>
-                <input class="table" type="password" placeholder="CONFIRM PASSWORD" required><br>
-                <input class="table" type="email" placeholder="EMAIL" required><br>
-                <div class="btns">
-                    <button class="OK" type="submit">REGISTER</button>
-                    <input type="button" class="REG" value="CANCEL" onclick="window.location.href = '/index.php'">
-                </div>
+                <form method="POST" action="register.php">
+                    <input class="table" type="text" name="reguser" placeholder="USERNAME" id="reguser"><br>
+                    <input class="table" type="password" name="regpass" placeholder="PASSWORD" id="regpass"><br>
+                    <input class="table" type="password" name="regpass2" placeholder="CONFIRM PASSWORD" id="regpass2"><br>
+                    <input class="table" type="email" name="regmail" placeholder="EMAIL" id="regmail"><br>
+                    <div class="btns">
+                        <input type="submit" class="OK" name="reg" id="reg" value="REGISTER">
+                        <input type="button" class="REG" value="CANCEL" onclick="window.location.href = '/index.php'">
+                    </div>
+                </form>
             </div>
             <div class="disclaimer">
                 <a>REGRAS:</a>
             </div>
             <div class="regra">
-                <a>1. É recomendado que a senha tenha entre 6 a 8 caracteres. Sendo um MAIÚSCULO, e um caractere especial (#, !, @).<a>
+                <a>1. É recomendado que a senha tenha no mínimo 8 caracteres e no máximo 32. Sendo um MAIÚSCULO, e um caractere especial (#, !, @).<a>
                 <br><br>
                 <a>2. Certifique-se de que possui acesso ao email usado para cadastro.</a>
                 <br><br>
