b2588b37bbb6579d47c8791db08485e0b94d724d
Andre Braga <andrebragagd@gmail.com>
2020-05-30 23:11:13 -0300

    Sistema de cadastro concluido

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+include("config.php");
+session_start();
+
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: main.php");
+    exit;
+}
+
+$username = $password = "";
+$username_err = $password_err = "";
 
 ?>
 <!DOCTYPE html>
--- register.php
+++ register.php
@@ -1,4 +1,54 @@
 <?php
+include("config.php");
+session_start();
+
+$username = '';
+$email ='';
+$errors = array(); 
+
+if(isset($_POST['reg'])){
+    $username = mysqli_real_escape_string($db, $_POST['reguser']);
+    $email = mysqli_real_escape_string($db, $_POST['regmail']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['regpass']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['regpass2']);
+
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($password_1)) { array_push($errors, "Password is required"); }
+    if ($password_1 != $password_2) {
+    array_push($errors, "The two passwords do not match");
+    }
+    
+    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+          array_push($errors, "Username already exists");
+          echo '<script language="javascript">';
+          echo 'alert("Usuário existente")';
+          echo '</script>';
+        }
+    
+        if ($user['email'] === $email) {
+          array_push($errors, "email already exists");
+          echo '<script language="javascript">';
+          echo 'alert("Email já cadastrado")';
+          echo '</script>';
+        }
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password_1);//encrypt the password before saving in the database
+  
+        $query = "INSERT INTO users (username, pword, email) 
+                  VALUES('$username', '$password', '$email')";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        header('location: index.php');
+    }
+  }
 
 ?>
 <!DOCTYPE html>
